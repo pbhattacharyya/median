e985017cfa898cdecd4ab015fc7688953070b7f4
pbhattacharyya <31600118+pbhattacharyya@users.noreply.github.com>
2018-06-21 19:12:19 -0400

    images-folder

--- components/CameraFrame.js
+++ components/CameraFrame.js
@@ -319,7 +319,8 @@ export default class CameraScreen extends React.Component {
 
         <View style={styles.containerWhole}>
           <View style={styles.imageArea}>
-            <Image source={require('./food.png')}  style={{width: 187, height: 376}} /> {/* half-face.png, bench.png, plate.png, food.png, tree.png */}
+            <Image source={require('../images/food.png')}  style={{width: 187, height: 376}} /> 
+            {/* half-face.png, bench.png, plate.png, food.png, tree.png */}
           </View> 
           <View style={styles.cameraArea}>
             <View style={{ flex: 1 }}>
